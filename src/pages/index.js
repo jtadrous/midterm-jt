6f5a4b1b3dcba2977d4c361ae2a914ead7ee695c
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2021-10-13 15:37:18 -0400

    Setup up menu and home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
 import React from "react";
-//import {Link} from 'gatsby';
+import "semantic-ui-css/semantic.css";
+import { Menu, Container, Segment, Header, Button } from "semantic-ui-react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <React.Fragment>
-      <h1>Home</h1>
+      <Container>
+        <Menu>
+          <Link to="/">
+            <Menu.Item icon="home"></Menu.Item>
+          </Link>
+          <Link to="/games">
+            <Menu.Item icon="game"></Menu.Item>
+          </Link>
+          <Link to="/contact">
+            <Menu.Item icon="users"></Menu.Item>
+          </Link>
+        </Menu>
+        <Segment textAlign="center">
+          <Header as="h1">Home</Header>
+          <p>Some random text that needs not readen.</p>
+          <Button color="green">Join Now!</Button>
+        </Segment>
+        <h2>News</h2>
+        <p>Some more random text</p>
+      </Container>
     </React.Fragment>
   );
 };
