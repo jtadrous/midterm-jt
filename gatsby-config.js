db13771ca6c1bbf4d8ff210c12cca7c765fd6cac
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2021-10-13 16:43:33 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl:
+      "https://in-info-web4.informatics.iupui.edu/~jtadrous/N322/midterm-jt",
     title: "midterm-jt",
   },
   plugins: [],
+  pathPrefix: "/~jtadrous/N322/midterm-jt",
 };
